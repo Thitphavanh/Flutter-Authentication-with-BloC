c3e7a9238dc34ccb2ca3a9fd65a657ab161954d3
hery <thitphavanh.psv@gmail.com>
2022-04-18 22:40:27 +0700

    update

--- lib/blocs/app/app_bloc.dart
+++ lib/blocs/app/app_bloc.dart
@@ -5,9 +5,11 @@ part 'app_event.dart';
 part 'app_state.dart';
 
 class AppBloc extends Bloc<AppEvent, AppState> {
-  AppBloc() : super(AppInitial()) {
-    on<AppEvent>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
+  AppBloc(AppState initialState) : super(initialState);
+//  final AuthRepository _authRepository;
+//   AppBloc({
+//     required AuthRepository authRepository,
+//   }) : _authRepository = authRepository, 
+//   super(authRepository.currentUser.isNotEmpty)
+ 
 }
